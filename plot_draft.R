5514b0a8ff8ca1f2a924f2efd8e52d6e593501c6
PickBranchZ <longfeiz@umich.edu>
2022-04-21 12:50:01 -0400

    update plot

--- plot_draft.R
+++ plot_draft.R
@@ -5,15 +5,15 @@ set.seed(342)
 center <- paste("center",1:19)
 x <- rnorm(19, mean=1, sd=0.2)
 se <- runif(19, 0.1, 0.5)
-lcl_x <- x - se
-ucl_x <- x + se
-tmp <- data.frame("center" = center, "d_ratio" = x, "lcl"=lcl_x, "ucl"=ucl_x)
+lci_x <- x - se
+uci_x <- x + se
+tmp <- data.frame("center" = center, "d_ratio" = x, "lci"=lci_x, "uci"=uci_x)
 
 
 myplot <- tmp %>% 
   arrange(d_ratio) %>%
   mutate(center=factor(center, levels=center)) %>%
-  ggplot(aes(y=center, x=d_ratio, xmin=lcl, xmax=ucl)) +
+  ggplot(aes(y=center, x=d_ratio, xmin=lci, xmax=uci)) +
   geom_vline(xintercept=1, linetype="dashed", size=0.5) +
   geom_errorbarh(height=0.15, size=.5) + 
   geom_point(size=1.5) +
